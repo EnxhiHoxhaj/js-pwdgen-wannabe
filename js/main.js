f86146ebcf1c55eb51c866bc99cdfbea63b3b491
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-03 16:40:09 +0200

    compilazione del form

--- js/main.js
+++ js/main.js
@@ -2,10 +2,26 @@
 
 //inserimento del nome
 
+let nomeUtente = "Enxhi";
+
+document.getElementById ("nome").innerHTML = "Inserisci il tuo nome: " + nomeUtente
+
 //inserimento del cognome
+
+let cognomeUtente = "Hoxhaj";
+
+document.getElementById ("cognome").innerHTML = "Inserisci il tuo cognome: " + cognomeUtente
 
 //inserimento del colore preferito
 
-//aggiunta del cognome
+let colorePreferito = "Verde";
+
+document.getElementById ("colore_p").innerHTML = "Qual'è il tuo colore preferito?: " + colorePreferito
+
+// inserimento del numero 23
+
+const numventitre = 23;
 
 //formulazione della password
+
+document.getElementById ("password").innerHTML = `La password che ti è stata assegnata è la seguente: ${nomeUtente}${cognomeUtente}${colorePreferito}${numventitre}`;
