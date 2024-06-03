400dcedeb25093c03c72c588b287c52fd1ffcfde
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-03 16:17:02 +0200

    Formulazione del layout e del diagramma di flusso

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//assegnazione di una password
+
+//inserimento del nome
+
+//inserimento del cognome
+
+//inserimento del colore preferito
+
+//aggiunta del cognome
+
+//formulazione della password
